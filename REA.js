f19ca574e452fb31c62796048f74d42c406db063
HaruGrossman <dogtaboodog@yahoo.com>
2023-04-10 14:06:59 -0500

    <firstSave>

--- REA.js
+++ REA.js
@@ -1,1 +1,13 @@
 
+//input element from html, save data and keep array
+
+// submit button that adds new property, add event listner
+    //adds to arr and maintains arr
+
+// loop through arr, and display on page
+    //foreach or sort
+
+
+// comment button add event listner
+    // comment text box pop up,
+        // submit save data and keep string (in arr attached to property)
